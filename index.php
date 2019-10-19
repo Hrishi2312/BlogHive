18174e471976bcd07e3a1c443616ceba0e66852c
Hrishi2312 <agrawalhrishikesh23@gmail.com>
2019-10-19 13:29:51 +0530

    Added About Us

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         <i class="fa fa-bars menu-toggle"></i>
         <ul class="nav">
             <li><a href="index.php">Home</a></li>
-            <li><a href="about.html">About</a></li>
+            <li><a href="about_us/index.html">About</a></li>
             <li><a href="contact.html">Contact</a></li>
             <?php
             if(isset($_SESSION['id'])):
